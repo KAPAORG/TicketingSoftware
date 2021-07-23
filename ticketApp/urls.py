2d6560ce2c7fb7df8e0f52edc5fdf0e9c0b38ba2
Ashutosh <ashutoshb2000@gmail.com>
2021-07-23 22:57:18 +0100

    a template to start

--- ticketApp/urls.py
+++ ticketApp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from ticketApp import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.dashboard),
+    path('my_open_issues/', views.my_open_issues),
+    path('reported_by_me/', views.reported_by_me),
+    path('create_ticket/', views.create_ticket),
+    path('my_projects/', views.my_projects),
 ]
--- ticketApp/views.py
+++ ticketApp/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render, HttpResponse, HttpResponsePermanentRedirect
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('base h ye bsdwalo')
+def dashboard(request):
+    return render(request, 'Dashboard.html')
+
+def create_ticket(request):
+    return render(request, 'create_ticket.html')
+
+def my_open_issues(request):
+    return render(request, 'my_open_issues.html')
+def reported_by_me(request):
+    return render(request, 'reported_by_me.html')
+
+def my_projects(request):
+    return render(request, 'my_projects.html')
