5fef40f45d94a361088435ae69b3849e10caaf8a
Ashutosh <ashutoshb2000@gmail.com>
2021-07-24 19:47:20 +0100

    mongodb setup

--- ticketApp/models.py
+++ ticketApp/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.db.models.fields import CharField
+
 # Create your models here.
 class tickets(models.Model):
-    project =CharField(max_length=20)
-    summary =CharField(max_length=200)
-    assignee =CharField(max_length=30, default="")
-    Status =CharField(max_length=10, default="")
-    
+    project = CharField(max_length=20)
+    summary = CharField(max_length=200)
+    type = CharField(max_length=10, default="")
+    assignee = CharField(max_length=30, default="")
+    Status = CharField(max_length=10, default="")
+
     def __str__(self):
-        return self.project 
+        return self.project
