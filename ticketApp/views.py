75aacd268f3c0427ad7d3dbe6ad1334e4cf02cc0
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2021-12-02 20:52:45 +0000

    initial django setup

--- ticketApp/views.py
+++ ticketApp/views.py
@@ -3,16 +3,5 @@ from django.shortcuts import render, HttpResponse, HttpResponsePermanentRedirect
 
 # Create your views here.
 
-def dashboard(request):
-    return render(request, 'Dashboard.html')
-
-def create_ticket(request):
-    return render(request, 'create_ticket.html')
-
-def my_open_issues(request):
-    return render(request, 'my_open_issues.html')
-def reported_by_me(request):
-    return render(request, 'reported_by_me.html')
-
-def my_projects(request):
-    return render(request, 'my_projects.html')
+def index(request):
+    return render(request, 'base.html')
